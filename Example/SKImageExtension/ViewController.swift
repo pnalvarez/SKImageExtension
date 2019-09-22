5ae4a80d6b2f30418e2da9247b463ce90e824561
Pedro Neves Alvarez <pedroalvarez@Pedros-MacBook-Pro-2.local>
2019-09-22 13:14:35 -0300

    Initial commit

---
+++ Example/SKImageExtension/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SKImageExtension
+//
+//  Created by pnalvarez on 09/22/2019.
+//  Copyright (c) 2019 pnalvarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
